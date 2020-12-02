fd42578f5946b4fbde73cd82b7ab8b52b2f9368f
Kneggalicious <kentuckyboris@gmail.com>
2020-12-02 10:24:54 +0100

    cleaned

--- day2/day2.py
+++ day2/day2.py
@@ -1,16 +1,15 @@
-input_values = []
-
 with open('day2input.txt') as f:
     input_values = list(f.readlines())
 
-# Debug values ---> values = ["9-12 q: qqqxhnhdmqqqqjz", "13-14 g: ggggghggccgqfjgggg", "11-13 j: jjjjsjdjnzglfjnjjjj"]
+    # values = ["9-12 q: qqqxhnhdmqqqqjz", "13-14 g: ggggghggccgqfjgggg", "11-13 j: jjjjsjdjnzglfjnjjjj"] <--- Debug values
 
-valid_password_counter = 0
-# Debug valued ---> for line in values:
-for line in input_values:
-    split_line = line.split(" ")
-    low_value, high_value = split_line[0].split("-")
-    counted_value = split_line[2].count(split_line[1].strip(":"))
-    if counted_value >= int(low_value) and counted_value <= int(high_value):
-        valid_password_counter += 1
-print(valid_password_counter)
+    valid_password_counter = 0
+    # for line in values: <--- Debug values
+    for line in input_values:
+        split_line = line.split(" ")
+        low_value, high_value = split_line[0].split("-")
+        counted_value = split_line[2].count(split_line[1].strip(":"))
+        if counted_value >= int(low_value) and counted_value <= int(high_value):
+            valid_password_counter += 1
+
+    print(valid_password_counter)
