be29089546b7601bb56fc02bbe78059ccae99e63
Kneggalicious <kentuckyboris@gmail.com>
2020-12-03 19:00:13 +0100

    Solution to part2 of day3

--- day3/day3.py
+++ day3/day3.py
@@ -4,19 +4,45 @@ with open('day3input.txt') as f:
     for line in input_values:
         stripped_values.append(line.strip("\n"))
 
-    trees = 0
-    x = 0
-    for y in range(len(stripped_values)):
-        if y == 0:
-            pass
-        else:
-            if x + 3 > len(stripped_values[y])-1:
-                x = (x + 3) - (len(stripped_values[y])) 
-                if stripped_values[y][x] == "#":
-                    trees += 1                
-            else: 
-                x += 3
-                if stripped_values[y][x] == "#":
-                    trees += 1
-    
-    print(trees)
+# part 1
+    def slope_calc(stepx):
+        trees = 0
+        x = 0
+        for y in range(0,len(stripped_values)):
+            if y == 0:
+                pass
+            else:
+                if x + stepx > len(stripped_values[y])-1:
+                    x = (x + stepx) - (len(stripped_values[y])) 
+                    if stripped_values[y][x] == "#":
+                        trees += 1                
+                else: 
+                    x += stepx
+                    if stripped_values[y][x] == "#":
+                        trees += 1
+        return trees
+
+# part 2
+    def slope_calc_y(stepx, stepy):
+        trees = 0
+        x = 0
+        for y in range(0,len(stripped_values), stepy):
+            if y == 0:
+                pass
+            else:
+                if x + stepx > len(stripped_values[y])-1:
+                    x = (x + stepx) - (len(stripped_values[y])) 
+                    if stripped_values[y][x] == "#":
+                        trees += 1                
+                else: 
+                    x += stepx
+                    if stripped_values[y][x] == "#":
+                        trees += 1
+        return trees
+
+    print(slope_calc(1))
+    print(slope_calc(3))
+    print(slope_calc(5))
+    print(slope_calc(7))
+    print(slope_calc_y(1, 2))
+
