c615d8e8d737e7db83a0cf21926c2efc2cbe37be
Kneggalicious <kentuckyboris@gmail.com>
2020-12-06 19:55:38 +0100

    day5 part 1 solution done

---
+++ day5/day5.py
@@ -0,0 +1,61 @@
+with open('day5input.txt') as txtfile:
+    input_values = [item.strip("\n") for item in txtfile]
+
+    rows = 128
+    columns = 8
+    seating_grid = []
+    for row in range(128):
+        seating_grid.append([])
+        for col in range(8):
+            seating_grid[row].append([col])
+
+    devValue = "FBFBBFFRLR"
+    devValue2 = ["FFFFFBBRLL", "FBFBBFFRLR"]
+
+    def getSeat(boardingpass):
+        FBvalue = boardingpass[:7]
+        LRvalue = boardingpass[7:]
+        rows = [[row] for row in range(128)]
+        columns = [[col] for col in range(8)]
+
+        while len(FBvalue) > 0:
+            sliceIndex = len(rows) // 2
+            if FBvalue[0] == "F":
+                FBvalue = FBvalue[1:]
+                rows = rows[:sliceIndex]
+
+            elif FBvalue[0] == "B":
+                FBvalue = FBvalue[1:]
+                rows = rows[sliceIndex:]
+
+            else:
+                print("Wrong value! got {} from {}".format(FBvalue[0], boardingpass))
+
+        while len(LRvalue) > 0:
+            sliceIndex = len(columns) // 2
+            if LRvalue[0] == "L":
+                LRvalue = LRvalue[1:]
+                columns = columns[:sliceIndex]
+
+            elif LRvalue[0] == "R":
+                LRvalue = LRvalue[1:]
+                columns = columns[sliceIndex:]
+
+            else:
+                print("Wrong value! got {} from {}".format(LRvalue[0], boardingpass))
+        return rows[0][0], columns[0][0], boardingpass
+
+    def getHighestID(list):
+        highestID = 0
+
+        for item in list:
+            r,c, boardingpass = getSeat(item)
+            newHigh = r * 8 + c
+            if newHigh > highestID:
+                highestID = newHigh
+
+        return highestID
+    
+    print(getSeat(devValue))
+    #part 1 solution under
+    print(getHighestID(input_values))
